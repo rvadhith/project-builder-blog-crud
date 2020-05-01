b24adcac18d3900af593b342d73712df02ef2980
Adhithya <rvadhith@gmail.com>
2020-05-01 09:27:31 +0530

    Final Commit

--- src/controller/BlogController.java
+++ src/controller/BlogController.java
@@ -14,7 +14,6 @@ import javax.servlet.http.HttpServletResponse;
 import model.Blog;
 import model.User;
 import service.CRUDOperations;
-import service.ExcelFileStorage;
 import utility.CheckBlogPost;
 
 
@@ -60,12 +59,12 @@ public class BlogController extends HttpServlet {
 		boolean check=checkBlog.checkBlog(blog);
 		
 		CRUDOperations crud=new CRUDOperations();
-		List<Blog> listblog = crud.createBlog(blog);
+		List<Blog> listblog = crud.addBlog(blog);
 
 		if(check) {
 			request.setAttribute("listBlog", listblog);
-//			request.setAttribute("blog", blog);
-//			request.setAttribute("user",user);
+			request.setAttribute("blog", blog);
+			request.setAttribute("user",user);
 			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/blogView.jsp");
 			rd.forward(request, response);
 		}
--- src/service/CRUDOperations.java
+++ src/service/CRUDOperations.java
@@ -7,23 +7,14 @@ import java.util.List;
 import model.Blog;
 
 public class CRUDOperations {
-	static List<Blog> list=new ArrayList<Blog>(); // Store the blog in the list
+	static List<Blog> blogCRUD = new ArrayList<Blog>(); // Store the blog in the list
 	
-	public List<Blog> createBlog(Blog blog) throws IOException {
-		list.add(blog);
-		return list;
+	public List<Blog> addBlog(Blog blog) throws IOException {
+		blogCRUD.add(blog);
+		return blogCRUD;
 	}
 	
 	public List<Blog> viewBlog(Blog blog) throws IOException{
-		return null;
-		
-	}
-	public List<Blog> updateBlog(Blog blog) throws IOException{
-		return null;
-	
-	}
-	public List<Blog> deleteBlog(Blog blog) throws IOException{
-		return null;
-		
+		return blogCRUD;		
 	}
 }
